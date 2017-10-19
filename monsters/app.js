cb020484c0b82e9518677298e32a98b013cf723e
Brian Fajardo <bmf5@hotmail.com>
2017-10-18 22:27:57 -0400

    Add conditional battle log on directive actions

--- monsters/app.js
+++ monsters/app.js
@@ -4,28 +4,43 @@ new Vue({
     gameStarted: false,
     playerHealth: 100,
     monsterHealth: 100,
+    turnLog: [],
   },
   methods: {
     initGame: function() {
       this.gameStarted = true
       this.playerHealth = 100
       this.monsterHealth = 100
+      this.turnLog = []
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10)
+      const damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turnLog.unshift({
+        text: `Player tickles Monster for ${damage} damage`,
+      })
       if (this.checkWin()) return
       this.monsterAttack()
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(5, 15)
+      const damage = this.calculateDamage(5, 15)
+      this.monsterHealth -= this.turnLog.unshift({
+        text: `Player crits the monster for ${damage} damage`,
+      })
       if (this.checkWin()) return
       this.monsterAttack()
     },
     heal: function() {
       if (this.playerHealth <= 90) {
         this.playerHealth += 20
+        this.turnLog.unshift({
+          text: 'Player healed for 20 HP',
+        })
       } else {
         this.playerHealth = 100
+        this.turnLog.unshift({
+          text: 'Player healed to full HP',
+        })
       }
       this.monsterAttack()
     },
@@ -33,19 +48,23 @@ new Vue({
       return Math.max(Math.floor(Math.random() * max) + 1, min)
     },
     monsterAttack: function() {
-      this.playerHealth -= this.calculateDamage(10, 16)
+      const damage = this.calculateDamage(10, 16)
+      this.playerHealth -= damage
+      this.turnLog.unshift({
+        text: `Monster smashes Player for ${damage} damage`,
+      })
       this.checkWin()
     },
     checkWin: function() {
       if (this.monsterHealth <= 0) {
-        if (confirm('You killed the monster. Victory!')) {
+        if (confirm('You killed the monster. VICTORY!')) {
           this.initGame()
         } else {
           this.gameStarted = false
         }
         return true
       } else if (this.playerHealth <= 0) {
-        if (confirm('You have been defeated. Try again.')) {
+        if (confirm('You have been defeated. Try again?')) {
           this.initGame()
         } else {
           this.gameStarted = false
